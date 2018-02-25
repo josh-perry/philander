551348cd9117c8a63a7881eaf7d38372b89add49
Josh Perry <joshperry@outlook.com>
2018-02-25 21:36:49 +0000

    Launch the game with the right binary

--- philander.py
+++ philander.py
@@ -3,6 +3,7 @@ LOVE version selector and downloader
 """
 import sys
 from pathlib import Path
+from subprocess import call
 
 from game import Game
 from love_downloader import LoveDownloader
@@ -19,6 +20,10 @@ def main():
 
     downloader = LoveDownloader(Platform.Windows)
     downloader.download(game.version)
+
+    binary = Path(downloader.get_version_folder_name(game.version))/"love.exe"
+
+    call([str("love" / binary), str(game.path)])
 
 
 if __name__ == '__main__':
